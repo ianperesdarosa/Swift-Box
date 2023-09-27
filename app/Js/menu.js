3fc586925100c77703e8070e160ef65638ef4d1f
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-09-26 21:51:17 -0300

    commit
    intregração de novas telas

--- app/Js/menu.js
+++ app/Js/menu.js
@@ -1,7 +1,7 @@
 function openNav() {
     document.getElementById("mySidepanel").style.width = "100%";
-  }
+}
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
-  }
+}
